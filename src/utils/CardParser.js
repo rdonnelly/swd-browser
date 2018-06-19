a3eb55b80a911e7c491432e5a332e9bc04f60458
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-06-18 22:03:52 -0400

    fix card text symbol regex

--- src/utils/CardParser.js
+++ src/utils/CardParser.js
@@ -2,23 +2,23 @@ import React from 'react';
 import SWDIcon from '../components/SWDIcon';
 
 const ICON_REPLACEMENTS = Object.freeze({
-  '[blank]': '<icon name="blank"></icon>',
-  '[discard]': '<icon name="discard"></icon>',
-  '[disrupt]': '<icon name="disrupt"></icon>',
-  '[focus]': '<icon name="focus"></icon>',
-  '[melee]': '<icon name="melee"></icon>',
-  '[ranged]': '<icon name="ranged"></icon>',
-  '[indirect]': '<icon name="indirect"></icon>',
-  '[shield]': '<icon name="shield"></icon>',
-  '[resource]': '<icon name="resource"></icon>',
-  '[special]': '<icon name="special"></icon>',
-  '[unique]': '<icon name="unique"></icon>',
-  '[AW]': '<icon name="aw"></icon>',
-  '[SoR]': '<icon name="sor"></icon>',
-  '[EaW]': '<icon name="eaw"></icon>',
-  '[TPG]': '<icon name="tpg"></icon>',
-  '[LEG]': '<icon name="leg"></icon>',
-  '[RIV]': '<icon name="riv"></icon>',
+  blank: '<icon name="blank"></icon>',
+  discard: '<icon name="discard"></icon>',
+  disrupt: '<icon name="disrupt"></icon>',
+  focus: '<icon name="focus"></icon>',
+  melee: '<icon name="melee"></icon>',
+  ranged: '<icon name="ranged"></icon>',
+  indirect: '<icon name="indirect"></icon>',
+  shield: '<icon name="shield"></icon>',
+  resource: '<icon name="resource"></icon>',
+  special: '<icon name="special"></icon>',
+  unique: '<icon name="unique"></icon>',
+  AW: '<icon name="aw"></icon>',
+  SoR: '<icon name="sor"></icon>',
+  EaW: '<icon name="eaw"></icon>',
+  TPG: '<icon name="tpg"></icon>',
+  LEG: '<icon name="leg"></icon>',
+  RIV: '<icon name="riv"></icon>',
 });
 
 export default {
@@ -36,7 +36,7 @@ export default {
   replaceIconPlaceholders(text) {
     let newText = text;
     Object.keys(ICON_REPLACEMENTS).forEach((key) => {
-      newText = newText.replace(new RegExp(key, 'gi'), ICON_REPLACEMENTS[key]);
+      newText = newText.replace(new RegExp(`\\[${key}\\]`, 'gi'), ICON_REPLACEMENTS[key]);
     });
 
     return newText;
