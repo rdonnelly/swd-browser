97a4a914c8b41f259ad41f0e68f35fc72c079158
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-06-21 23:42:54 -0400

    add darker white translucent

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -32,6 +32,7 @@ const darkGrayDark = 'rgba(44, 62, 80, 1.0)';
 
 const white = 'rgba(255, 255, 255, 1.0)';
 const whiteTranslucent = 'rgba(255, 255, 255, 0.5)';
+const whiteTranslucentDark = 'rgba(255, 255, 255, 0.75)';
 
 export default {
   turquoise,
@@ -68,6 +69,7 @@ export default {
 
   white,
   whiteTranslucent,
+  whiteTranslucentDark,
 
   brand: turquoise,
   brandDark: turquoiseDark,
