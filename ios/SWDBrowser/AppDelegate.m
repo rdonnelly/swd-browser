96aba2ff05e883a9ca8c70c73c5765ec3ca9f1ca
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-06-21 23:43:50 -0400

    fix launch screen content mode

--- ios/SWDBrowser/AppDelegate.m
+++ ios/SWDBrowser/AppDelegate.m
@@ -23,35 +23,35 @@
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:236.0/255.0 green:240.0/255.0 blue:241.0/255.0 alpha:1.0];
-  
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
-  
+
+
   UIView *loadingView = [[UIView alloc] initWithFrame:rootView.bounds];
   [loadingView setBackgroundColor:[[UIColor alloc] initWithRed:236.0/255.0 green:240.0/255.0 blue:241.0/255.0 alpha:1.0]];
-  
-  
+
+
   UIImageView* splashView = [[UIImageView alloc] initWithFrame:loadingView.bounds];
   [splashView setImage:[UIImage imageNamed:@"launch-browser"]];
-  [splashView setContentMode:UIViewContentModeScaleAspectFit];
+  [splashView setContentMode:UIViewContentModeCenter];
   [loadingView addSubview:splashView];
-  
-  
+
+
   UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
   spinner.color = [[UIColor alloc] initWithRed:26.0/255.0 green:188.0/255.0 blue:156.0/255.0 alpha:1.0];
   spinner.center = CGPointMake(loadingView.frame.size.width * 0.5, loadingView.frame.size.height * 0.85);
   [spinner startAnimating];
   [loadingView addSubview:spinner];
-  
-  
+
+
   rootView.loadingView = loadingView;
   rootView.loadingViewFadeDelay = 0.5;
   rootView.loadingViewFadeDuration = 0.5;
-  
+
   return YES;
 }
 
