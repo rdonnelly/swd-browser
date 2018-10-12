dea225a56d1806fc33344f98d493ea5efc6ec51c
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-10-12 00:06:57 -0400

    update dependencies and upgrade react native

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { AppRegistry } from 'react-native';
+
+import { name as appName } from './app.json';
 import App from './src/App';
 import Setup from './src/Setup';
 
 Setup.init();
 
-AppRegistry.registerComponent('SWDBrowser', () => App);
+AppRegistry.registerComponent(appName, () => App);
