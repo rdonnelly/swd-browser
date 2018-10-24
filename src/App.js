a8aaf0d0e7563a95c0da89fd44af246820bc59b7
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-10-24 00:19:20 -0400

    use react native screens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { useScreens } from 'react-native-screens';
 
 import MainTabNavigator from './navigation/MainTabNavigator';
+
+useScreens();
 
 export default class App extends React.Component {
   componentDidMount() {
