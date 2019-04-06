1eba49568ab1f9a48e63c7bb7bace66ac7303b6a
Ryan Donnelly <ryanjdonnelly@gmail.com>
2019-04-05 23:31:44 -0400

    fix types sorting and subtype none

--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,27 @@
 import factionsRaw from 'swdestinydb-json-data/factions.json';
 import subtypesRaw from 'swdestinydb-json-data/subtypes.json';
+import typesRaw from 'swdestinydb-json-data/types.json';
 
 export const factions = factionsRaw.map((faction) => ({
   code: faction.code,
   name: faction.code.charAt(0).toUpperCase() + faction.code.slice(1),
 }));
 
-export const subtypes = [...subtypesRaw, { code: '', name: 'No Subtype' }];
+export const subtypes = [...subtypesRaw, { code: 'none', name: 'No Subtype' }];
+export const types = [...typesRaw].sort((a, b) => {
+  if (a.name < b.name) {
+    return -1;
+  }
+
+  if (a.name > b.name) {
+    return 1;
+  }
+
+  return 0;
+});
 
 export { default as affiliations } from 'swdestinydb-json-data/affiliations.json';
 export { default as rarities } from 'swdestinydb-json-data/rarities.json';
 export { default as sets } from 'swdestinydb-json-data/sets.json';
-export { default as types } from 'swdestinydb-json-data/types.json';
 
 export { default as cardDatabase } from './cardDatabase';
