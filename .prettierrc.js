2a5e26aabac117bdee47897f1dd9e6533723c8f8
Ryan Donnelly <ryanjdonnelly@gmail.com>
2019-12-29 13:42:14 -0500

    upgrade react native to 61

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// {
+//   "arrowParens": "always",
+//   "printWidth": 120,
+//   "singleQuote": true,
+//   "jsxBracketSameLine": false,
+//   "trailingComma": "all"
+// }
+
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
